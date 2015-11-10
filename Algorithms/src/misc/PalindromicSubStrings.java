866abe174ce08d6ca65f81091bb684cc41cd1306
Sibani Prasad Dash <sibani.dash@kony.com>
2015-11-11 03:03:35 +0530

    finding longest palindrome code changed

--- Algorithms/src/misc/PalindromicSubStrings.java
+++ Algorithms/src/misc/PalindromicSubStrings.java
@@ -1,112 +1,53 @@
 package misc;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class PalindromicSubStrings {
-  /*
- * Complete the function below.
- */
-	static boolean isOdd(int number){
-		return (number & 1) == 1;
-	}
-    static boolean isPalindrome(String str){
-        int start = 0, end = str.length() - 1;
-        while(start < end){
-            if(str.charAt(start) != str.charAt(end)){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+public class PalindromicSubStrings {    
+    public static int findLongestPalindrome(String str){
+    	int max = 0;
+    	if(str == null){
+    		return max;
+    	}
+    	int strLen = str.length();
+    	int left = 0,right = 0;
+    	for(int i = 0; i < strLen; i++){
+    		int maxPal = findMaxPalindromeCenteredAt(str, i, i);
+    		if(maxPal > max){
+    			max = maxPal;
+    			left = i - max / 2;
+    			right = i + max / 2;
+    		}
+    		maxPal = findMaxPalindromeCenteredAt(str, i, i + 1);
+    		if(maxPal > max){
+    			max = maxPal;
+    			left = i - (max - 2)/2;
+    			right = i + 1 + (max - 2)/2;
+    		}
+    	}
+    	System.out.println(str.substring(left, right + 1));
+    	return max;
     }
-    
-    static boolean isPalindrome(String str, int centerIndex,int startIndex, int endIndex){
-        int start = 0, end = str.length() - 1;
-        while(start < end){
-            if(str.charAt(start) != str.charAt(end)){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-    
-    static int countPalindromesCenteredAt(String str, int centerIndex, int modifiedLength, HashMap<String, Integer> lookUpMap){
-        int count = 0;
-        int window = (centerIndex <= modifiedLength - centerIndex - 1) ? centerIndex : modifiedLength - centerIndex - 1;
-        int step = isOdd(centerIndex) ? 1 : 0;
-        for(; step <= window; step += 2){
-            int startIndex = (centerIndex - step) >> 1;
-            int endIndex = (centerIndex + step) >> 1;
-            String subStr = str.substring(startIndex, endIndex + 1);
-            if(isPalindrome(subStr)){
-                if(lookUpMap.get(subStr) == null){
-                    count++;
-                    lookUpMap.put(subStr, 1);
-                    System.out.println(subStr);
-                }else{
-                    lookUpMap.put(subStr, lookUpMap.get(subStr) + 1);
-                }
-            }
-        }
-        return count;
-    }
-    static int palindrome(String str) {
-        int str_len = str.length();
-        int count = 0;
-        HashMap<String, Integer> distintSubPalindromes = new HashMap<String, Integer>();
-        int modifiedLength = 2 * str_len - 1;
-        for(int i=0; i < modifiedLength; i++){
-            count+= countPalindromesCenteredAt(str, i, modifiedLength, distintSubPalindromes);
-        }
-        System.out.println(count);
-        return count;
-    }
-    
-    public static int longestPalindromicSubString(String str, int centerIndex, int modifiedLength){
-    	int window = (centerIndex <= modifiedLength - centerIndex - 1) ? centerIndex : modifiedLength - centerIndex - 1;
-        int step = isOdd(centerIndex) ? 1 : 0;
-        int maxLength = 0;;
-        boolean isPalindrome = true;
-        for(; isPalindrome && step <= window; step += 2){
-            int startIndex = (centerIndex - step) >> 1;
-            int endIndex = (centerIndex + step) >> 1;
-        	if(str.charAt(startIndex) == str.charAt(endIndex)){
-        		maxLength = endIndex - startIndex + 1;
-        	}else {
-				isPalindrome = false;
+
+    private static int findMaxPalindromeCenteredAt(String str, int leftCentre, int rightCentre) {
+		int length = str.length();
+		int maxPalCentredAt = 0; 
+    	while(leftCentre >= 0 && rightCentre <= length - 1){
+    		if(str.charAt(leftCentre) == str.charAt(rightCentre)){
+    			maxPalCentredAt = rightCentre - leftCentre + 1;
+    			leftCentre--;
+    			rightCentre++;
+    		}else {
+				break;
 			}
-        }
-        return maxLength;
-    }
-    
-    public static void printLongestPalindromicSubString(String str){
-    	int totalMax = 1,max = 0;
-    	int modifiedLength = 2 * str.length() - 1;
-    	int originalIndex = 0;
-        for(int i = 0; i < modifiedLength; i++){
-            max = longestPalindromicSubString(str, i, modifiedLength);
-            if(max > totalMax){
-            	originalIndex = i >> 1;
-            	totalMax = max;
-            }
-        }
-        int leftIndex = originalIndex - (totalMax - (totalMax >> 1) - 1);
-        int rightIndex = originalIndex + (totalMax >> 1);
-        System.out.println(str.substring(leftIndex, rightIndex + 1));
-        System.out.println(totalMax);
-    }
-    
-    public static void main(String[] args) throws IOException{
+    	}
+		return maxPalCentredAt;
+	}
+	public static void main(String[] args) throws IOException{
     	System.out.println("Enter input: ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        //palindrome(input);
-        printLongestPalindromicSubString(input);
+        System.out.println(findLongestPalindrome(input));
         in.close();
     }
 }
