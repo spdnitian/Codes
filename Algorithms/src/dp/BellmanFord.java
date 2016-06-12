8383f43996841244947a3b4c962333d49f04d4f5
spdni <spdni@RPSD>
2016-06-13 01:39:31 +0530

    Added BellmanForm Algorithm

--- Algorithms/src/dp/BellmanFord.java
+++ Algorithms/src/dp/BellmanFord.java
@@ -8,10 +8,14 @@ class Graph{
 	int edges;
 	ArrayList<Integer[]> edgesMatrix;
 	
+	int[] shortestPathParentIndices;
+	
 	public Graph(int V, int E) {
 		vertices = V;
 		edges = E;
 		edgesMatrix = new ArrayList<Integer[]>(E);
+		shortestPathParentIndices = new int[vertices];
+		Arrays.fill(shortestPathParentIndices, Integer.MIN_VALUE);
 	}
 	
 	public void addEdge(int src, int dest, int weight) {
@@ -27,17 +31,36 @@ public class BellmanFord {
 			dists[i] = Integer.MAX_VALUE; // Infinity
 		}
 		dists[source] = 0;
-		for(int i = 0; i <= dists.length - 1; i++){
+		graph.shortestPathParentIndices[source] = -1;
+		for(int atMostEdge = 1; atMostEdge <= graph.vertices - 1; atMostEdge++){
 			for (Integer[] edge : graph.edgesMatrix) {
 				int src = edge[0], dest = edge[1], weight = edge[2];
 				if(dists[src] != Integer.MAX_VALUE && dists[dest] > dists[src] + weight){
 					dists[dest] = dists[src] + weight;
+					graph.shortestPathParentIndices[dest] = src;
 				}
 			}
+		}
+		for (int dest = 0; dest < dists.length; dest++) {
+			System.out.print(source+ " to " + dest + " : ");
+			printPathsFromSource(graph, dest);
+			System.out.println();
 		}
 		return dists;
 	}
 	
+	private static void printPathsFromSource(Graph graph, int dest) {
+		int parent = graph.shortestPathParentIndices[dest];
+		if(parent != -1 && parent != Integer.MIN_VALUE){
+			printPathsFromSource(graph, parent);
+		}
+		if(parent != Integer.MIN_VALUE){
+			System.out.print(dest + " ");
+		}else{
+			System.out.print("No Path.");
+		}
+	}
+
 	public static void main(String[] args) {
 		Graph graph = new Graph(5, 8);
 		graph.addEdge(0, 1, -1);
